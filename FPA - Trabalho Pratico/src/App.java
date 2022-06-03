054d80b0bc0556e9c6b0ce4945bff9c639f3f142
Octavio Martins <octaviomartins10@gmail.com>
2022-06-02 22:52:18 -0300

    Subindo trabalho de FPA

---
+++ FPA - Trabalho Pratico/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+// Criar gerador de array de números aleatorios;
+
+// Imprimir chave não ordenada se o usuário quiser;
+
+// Imprimir as chaves ordenadas, se o usuário desejar; 
+
+// Metodo para ordenar Seleção, Inserção, Quicksort, Heapsort;
+
+// Criar um jeito de contar o tempo de processamento de cada algorítmo
+
+// Contador dentro dos algorítmos, para pegar o número de comparações e de execução
